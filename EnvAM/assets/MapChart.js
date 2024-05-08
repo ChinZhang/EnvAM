0cb6072481f6c013a0fdd3c88de0a3d9ecbe1c0e
Fahim Tran <46301587+fahimtran@users.noreply.github.com>
2024-05-07 20:54:10 -0400

    ENVAM-1 - Fix MapChart.js, added libraries on frontend + backend.

--- EnvAM/assets/MapChart.js
+++ EnvAM/assets/MapChart.js
@@ -3,46 +3,53 @@ import {
   ZoomableGroup,
   ComposableMap,
   Geographies,
-  Geography
+  Geography,
 } from "react-simple-maps";
 import { scaleQuantile } from "d3-scale";
 import { csv } from "d3-fetch";
 import { max } from "d3-array";
-import dataset from "./dataset.csv"
+import dataset from "./dataset.csv";
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const geoFile = "./static/features.json";
 
 const MapChart = ({ setTooltipContent, series, year }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     // https://www.bls.gov/lau/
-    csv(dataset).then(dataset => {
+    csv(dataset).then((dataset) => {
       setData(dataset);
     });
   }, []);
   return (
     <>
-      <ComposableMap data-tip=""
-      projectionConfig={{
-        rotate: [-40, 0, 0],
-        scale: 180
-      }}
-      style={{
-        maxHeight: "600",
-        paddingLeft: '25%',
-        paddingRight: '25%'
-      }}
+      <ComposableMap
+        data-tip=""
+        projectionConfig={{
+          rotate: [-40, 0, 0],
+          scale: 180,
+        }}
+        style={{
+          maxHeight: "600",
+          paddingLeft: "25%",
+          paddingRight: "25%",
+        }}
       >
         <ZoomableGroup>
-          <Geographies geography={geoUrl}>
+          <Geographies geography={geoFile}>
             {({ geographies }) =>
-              geographies.map(geo => {
-                const cur = data.find(s => s.Country_Name === geo.properties.NAME &&
-                s.Series_Name === series)
+              geographies.map((geo) => {
+                const cur = data.find(
+                  (s) =>
+                    s.Country_Name === geo.properties.name &&
+                    s.Series_Name === series
+                );
                 const colorScale = scaleQuantile()
-                  .domain(data.filter(d => d.Series_Name === series).map(d => d[year]))
+                  .domain(
+                    data
+                      .filter((d) => d.Series_Name === series)
+                      .map((d) => d[year])
+                  )
                   .range([
                     "#ffedea",
                     "#ffcec5",
@@ -52,7 +59,7 @@ const MapChart = ({ setTooltipContent, series, year }) => {
                     "#e2492d",
                     "#be3d26",
                     "#9a311f",
-                    "#782618"
+                    "#782618",
                   ]);
                 return (
                   <Geography
@@ -60,33 +67,34 @@ const MapChart = ({ setTooltipContent, series, year }) => {
                     geography={geo}
                     onMouseEnter={() => {
                       if (cur) {
-                        setTooltipContent(geo.properties.NAME+": "+cur[year]);
+                        setTooltipContent(
+                          geo.properties.name + ": " + cur[year]
+                        );
                         if (!cur[year]) {
-                          setTooltipContent(geo.properties.NAME+": No data");
+                          setTooltipContent(geo.properties.name + ": No data");
                         }
-                      }
-                      else {
-                        setTooltipContent(geo.properties.NAME+": No data");
+                      } else {
+                        setTooltipContent(geo.properties.name + ": No data");
                       }
                     }}
                     onMouseLeave={() => {
                       setTooltipContent("");
                     }}
-                    stroke='#aaa'
-                    strokeWidth='0.5'
+                    stroke="#aaa"
+                    strokeWidth="0.5"
                     fill={cur && cur[year] ? colorScale(cur[year]) : "#EEE"}
                     style={{
                       default: {
-                        outline: 'none'
+                        outline: "none",
                       },
                       hover: {
                         fill: "#c9e4ca",
-                        outline: 'none'
+                        outline: "none",
                       },
                       pressed: {
                         fill: "#E42",
-                        outline: 'none'
-                      }
+                        outline: "none",
+                      },
                     }}
                   />
                 );
